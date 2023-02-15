dc68d65437466175584c4e8933349f63f843b938
null <me@github0null.io>
2023-02-15 09:23:27 +0800

    array.h: add array_clear

--- array.h
+++ array.h
@@ -25,6 +25,7 @@ before you include this file in *one* C/C++ file to create the implementation.
 #define array_create( type ) ARRAY_CAST( (void*)internal_array_create( sizeof( type ), NULL ) )
 #define array_create_memctx( type, memctx ) ARRAY_CAST( (void*)internal_array_create( sizeof( type ), (memctx) ) )
 #define array_destroy( array ) internal_array_destroy( (struct internal_array_t*) (array) )
+#define array_clear( array ) internal_array_clear( (struct internal_array_t*) (array) )
 #define array_add( array, item ) ARRAY_CAST( internal_array_add( (struct internal_array_t*) (array), (void*) (item), (int)sizeof( *item ) ) )
 #define array_remove( array, index ) internal_array_remove( (struct internal_array_t*) (array), (index) )
 #define array_remove_ordered( array, index ) internal_array_remove_ordered( (struct internal_array_t*) (array), (index) )
@@ -71,6 +72,7 @@ struct internal_array_t;
 
 struct internal_array_t* internal_array_create( int item_size, void* memctx );
 void internal_array_destroy( struct internal_array_t* array );
+void internal_array_clear( struct internal_array_t* array );
 void* internal_array_add( struct internal_array_t* array, void* item, int item_size );
 void internal_array_remove( struct internal_array_t* array, int index );
 void internal_array_remove_ordered( struct internal_array_t* array, int index );
@@ -158,7 +160,7 @@ struct internal_array_t* internal_array_create( int item_size, void* memctx ) {
     struct internal_array_t* array = (struct internal_array_t*) ARRAY_MALLOC( memctx, sizeof( struct internal_array_t ) );
     array->memctx = memctx;
     array->item_size = item_size;
-    array->capacity = 256;
+    array->capacity = 64;
     array->count = 0;
     array->items = ARRAY_MALLOC( memctx, (size_t) array->capacity * item_size );
     return array;
@@ -170,6 +172,9 @@ void internal_array_destroy( struct internal_array_t* array ) {
     ARRAY_FREE( array->memctx, array );
 }
 
+void internal_array_clear( struct internal_array_t* array ) {
+    array->count = 0;
+}
 
 void* internal_array_add( struct internal_array_t* array, void* item, int item_size  ) {
     ARRAY_ASSERT( item_size == array->item_size, "Invalid item" );
